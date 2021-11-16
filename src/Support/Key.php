7bb03b0c12a9a1e554535938da3fd6ff1dd6ceee
Andrey Helldar <helldar@ai-rus.com>
2021-11-16 18:36:06 +0300

    Removed code duplication

--- src/Support/Key.php
+++ src/Support/Key.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace DragonCode\Cache\Support;
 
-use DragonCode\Support\Facades\Helpers\Ables\Arrayable;
+use DragonCode\Cache\Concerns\Arrayable;
 
 class Key
 {
+    use Arrayable;
+
     public function get(string $separator, array $values): string
     {
         $hashed = $this->hash($values);
@@ -17,11 +19,9 @@ class Key
 
     protected function hash(array $values): array
     {
-        return Arrayable::of($values)
-            ->flatten()
-            ->map(static function ($value) {
-                return md5((string) $value);
-            })->get();
+        return $this->arrayMap($values, static function ($value) {
+            return md5((string) $value);
+        });
     }
 
     protected function compile(array $values, string $separator): string
